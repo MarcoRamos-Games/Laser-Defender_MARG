69ea1a11ea1e672f3a2ba745480e28d611a5a208
MarcoRamos99 <marcoramosg928@gmail.com>
2021-01-03 19:19:41 -0600

    Laser Defender Course Version

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy")]
+    [SerializeField] float health = 100f;
+    [SerializeField] int scoreValue = 150;
+
+    [Header("Shooting")]
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
+    [Header("VFX")]
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip enemyDead;
+    [SerializeField] AudioClip enemyShooting;
+    [SerializeField] [Range(0,1)] float volumeDead = 1f;
+    [SerializeField] [Range(0, 1)] float volumeShooting = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+        
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0f)
+        {
+            Fire();
+            AudioSource.PlayClipAtPoint(enemyShooting, Camera.main.transform.position, volumeShooting);
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(explosionVFX, transform.position, Quaternion.identity) ;
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(enemyDead, Camera.main.transform.position, volumeDead);
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 5f;
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadGameOver()
+    {
+        StartCoroutine(DelayLevelLoad());
+    }
+
+    public void LoadSpecialThanks()
+    {
+        SceneManager.LoadScene("Special Thanks");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator DelayLevelLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("Game Over");
+    }
+}
